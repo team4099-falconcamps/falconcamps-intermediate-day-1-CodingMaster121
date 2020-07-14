34c316be14a08bd3d342a288b1a97cdc2fba7dfe
CodingMaster121 <ytryanlo@gmail.com>
2020-07-14 17:17:01 +0000

    Added Fields for RobotContainer

--- src/main/java/com/team4099/falconcamps/RobotContainer.java
+++ src/main/java/com/team4099/falconcamps/RobotContainer.java
@@ -1,6 +1,13 @@
 package com.team4099.falconcamps;
 
+import com.team4099.falconcamps.subsystems.ShooterPIDSubsystem;
+import com.team4099.falconcamps.commands.shooter.ShooterPIDSubsystemCommand;
+import com.team4099.falconcamps.commands.shooter.ShooterPIDSubsystemIdleCommand;
+
 public class RobotContainer {
+    private ShooterPIDSubsystem shooterSubsystem;
+    private ShooterPIDSubsystemCommand shooterCommand;
+    private ShooterPIDSubsystemIdleCommand shooterIdleCommand;
     public RobotContainer() {
 
     }
--- src/main/java/com/team4099/falconcamps/commands/shooter/ShooterPIDSubsystemCommand.java
+++ src/main/java/com/team4099/falconcamps/commands/shooter/ShooterPIDSubsystemCommand.java
@@ -3,10 +3,10 @@ package com.team4099.falconcamps.commands.shooter;
 import com.team4099.falconcamps.subsystems.ShooterPIDSubsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class ShootPIDSubsystemCommand extends CommandBase {
+public class ShooterPIDSubsystemCommand extends CommandBase {
     private final ShooterPIDSubsystem shooter;
 
-    public ShootPIDSubsystemCommand(ShooterPIDSubsystem shooter) {
+    public ShooterPIDSubsystemCommand(ShooterPIDSubsystem shooter) {
         this.shooter = shooter;
         this.addRequirements(shooter);
     }
