17ac0ff3eb24e779804a8e78900153a76bf39b9a
CodingMaster121 <ytryanlo@gmail.com>
2020-07-14 18:46:28 +0000

    Added Code for ShooterSpark Option

--- src/main/java/com/team4099/falconcamps/RobotContainer.java
+++ src/main/java/com/team4099/falconcamps/RobotContainer.java
@@ -1,16 +1,22 @@
 package com.team4099.falconcamps;
 
 import com.team4099.falconcamps.subsystems.ShooterPIDSubsystem;
+import com.team4099.falconcamps.subsystems.ShooterSparkPID;
 import com.team4099.falconcamps.commands.shooter.ShooterPIDSubsystemCommand;
+import com.team4099.falconcamps.commands.shooter.ShooterSparkPIDCommand;
+import com.team4099.falconcamps.commands.shooter.ShooterSparkPIDIdleCommand;
 import com.team4099.falconcamps.commands.shooter.ShooterPIDSubsystemIdleCommand;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class RobotContainer {
     private ShooterPIDSubsystem shooterSubsystem = new ShooterPIDSubsystem();
+    private ShooterSparkPID shooterSubsystem1 = new ShooterSparkPID();
     private XboxController driverController = new XboxController(0);
     public RobotContainer() {
         new Trigger(driverController::getAButton).whenActive(new ShooterPIDSubsystemCommand(shooterSubsystem));
         shooterSubsystem.setDefaultCommand(new ShooterPIDSubsystemIdleCommand(shooterSubsystem));
+        new Trigger(driverController::getAButton).whenActive(new ShooterSparkPIDCommand(shooterSubsystem1));
+        shooterSubsystem1.setDefaultCommand(new ShooterSparkPIDIdleCommand(shooterSubsystem1));
     }
 }
--- src/main/java/com/team4099/falconcamps/commands/shooter/ShooterSparkPIDCommand.java
+++ src/main/java/com/team4099/falconcamps/commands/shooter/ShooterSparkPIDCommand.java
@@ -3,16 +3,16 @@ package com.team4099.falconcamps.commands.shooter;
 import com.team4099.falconcamps.subsystems.ShooterSparkPID;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class ShootSparkPIDCommand extends CommandBase {
+public class ShooterSparkPIDCommand extends CommandBase {
     private final ShooterSparkPID shooter;
 
-    public ShootSparkPIDCommand(ShooterSparkPID shooter) {
+    public ShooterSparkPIDCommand(ShooterSparkPID shooter) {
         this.shooter = shooter;
         this.addRequirements(shooter);
     }
 
     @Override
     public void initialize() {
-
+        shooter.setTargetVelocity(5000);
     }
 }
