7dfe099fc7a8cd2943d633936a187fad2e4e8a9c
CodingMaster121 <ytryanlo@gmail.com>
2020-07-14 17:23:48 +0000

    Added Button for the Shooter

--- src/main/java/com/team4099/falconcamps/RobotContainer.java
+++ src/main/java/com/team4099/falconcamps/RobotContainer.java
@@ -3,12 +3,14 @@ package com.team4099.falconcamps;
 import com.team4099.falconcamps.subsystems.ShooterPIDSubsystem;
 import com.team4099.falconcamps.commands.shooter.ShooterPIDSubsystemCommand;
 import com.team4099.falconcamps.commands.shooter.ShooterPIDSubsystemIdleCommand;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class RobotContainer {
     private ShooterPIDSubsystem shooterSubsystem;
-    private ShooterPIDSubsystemCommand shooterCommand;
-    private ShooterPIDSubsystemIdleCommand shooterIdleCommand;
     public RobotContainer() {
-
+        XboxController driverController = new XboxController(0);
+        new Trigger(driverController::getAButton).whenActive(new ShooterPIDSubsystemCommand(shooterSubsystem));
+        shooterSubsystem.setDefaultCommand(new ShooterPIDSubsystemIdleCommand(shooterSubsystem));
     }
 }
