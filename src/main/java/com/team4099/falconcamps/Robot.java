4e7e5e3dbe076dc776aa50a857708ce69db03265
CodingMaster121 <ytryanlo@gmail.com>
2020-07-15 17:55:00 +0000

    Added Code for Robot.java

--- src/main/java/com/team4099/falconcamps/Robot.java
+++ src/main/java/com/team4099/falconcamps/Robot.java
@@ -1,12 +1,20 @@
 package com.team4099.falconcamps;
 
+import com.team4099.falconcamps.subsystems.ShooterPIDSubsystem;
+import com.team4099.falconcamps.subsystems.ShooterSparkPID;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import io.github.oblarg.oblog.Logger;
+import io.github.oblarg.oblog.annotations.Log;
 
 public class Robot extends TimedRobot {
-    private final RobotContainer robotContainer = new RobotContainer();
+    @Log private final RobotContainer robotContainer = new RobotContainer();
+    @Log private final ShooterSparkPID shooterSparkSubsystem = new ShooterSparkPID();
+    @Log private final ShooterPIDSubsystem shooterPIDSubsystem = new ShooterPIDSubsystem();
 
-    @Override public void robotInit() {}
+    @Override public void robotInit() {
+        Logger.configureLoggingAndConfig(this, false);
+    }
 
     @Override public void autonomousInit() {}
 
@@ -16,6 +24,7 @@ public class Robot extends TimedRobot {
 
     @Override public void robotPeriodic() {
         CommandScheduler.getInstance().run();
+        Logger.updateEntries();
     }
 
     @Override public void autonomousPeriodic() {}
